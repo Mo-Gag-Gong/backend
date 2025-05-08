927421f27de26c10201e2c1624bc19accf452588
taehun2123 <pyoneng_@naver.com>
2025-05-08 11:53:33 +0900

    init project

---
+++ src/main/java/com/mogacko/mogacko/MogackoApplication.java
@@ -0,0 +1,13 @@
+package com.mogacko.mogacko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogackoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MogackoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mogacko/mogacko/MogackoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mogacko.mogacko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogackoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
